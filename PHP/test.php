8360a0bcfad31a1c111c53a0ccb6033fb147099f
18229905 <18229905@life.hkbu.edu.hk>
2021-04-08 12:56:15 +0800

    test php v2

--- PHP/test.php
+++ PHP/test.php
@@ -4,6 +4,7 @@ $username = "comp4107_grp08";
 $password = "246186";
 $dbname = "comp4107_grp08";
 $conn = new mysqli($servername, $username, $password, $dbname);
+$account = "";
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -25,25 +26,67 @@ if (strcmp($req->msgType, "LoginReq") === 0) {
     $reply->msgType = "LoginReply";
     $reply->cred = "Wrong Password";
   }
+
 //
 }else if (strcmp($req->msgType, "GetAccReq") === 0) {
+  $sql = "select PK_cardID from card where PK_cardID = '$req->cardNo'";
+  $account = mysqli_query($db,$sql);
   $reply->msgType = "GetAccReply";
+  $reply->cardNo = $req->cardNo;
+  $reply->cred = "Get account success";
+  $reply->accounts = $account;
 
 
 } else if (strcmp($req->msgType, "WithdrawReq") === 0) {
-  $reply->msgType = "WithdrawReply";
-
-
+  $sql = "select balance from card where PK_cardID = '$req->cardNo'";
+  $balance = mysqli_query($db,$sql);
+  //$row = mysqli_fetch_array($balance);
+  if($balance > $req->amount){
+    $sql = "update card set balance = '$balance' - '$req->amount' where PK_cardID = cardID";
+    $reply->msgType = "WithdrawReply";
+    $reply->cardNo = $req->cardNo;
+    $reply->accNo = $account;
+    $reply->cred = "Withdraw Success";
+  }else(){
+    $reply->msgType = "WithdrawReply";
+    $reply->cred = "Not enough money";
+  }
+  
+  
 } else if (strcmp($req->msgType, "DepositReq") === 0) {
+  $sql = "select balance from card where PK_cardID = '$req->cardNo'";
+  $balance = mysqli_query($db,$sql);
+  $sql = "update card set balance = '$balance' + '$req->amount' where PK_cardID = cardID";
   $reply->msgType = "DepositReply";
-
+  $reply->cardNo = $req->cardNo;
+  $reply->accNo = $account;
+  $reply->cred = "Deposit Success";
 
 } else if (strcmp($req->msgType, "EnquiryReq") === 0) {
+  $sql = "select balance from card where PK_cardID = '$req->cardNo'";
+  $balance = mysqli_query($db,$sql);
   $reply->msgType = "EnquiryReply";
-
+  $reply->cardNo = $req->cardNo;
+  $reply->accNo = $account;
+  $reply->balance = $balance;
+  $reply->cred = "Enquiry Success";
 
 } else if (strcmp($req->msgType, "TransferReq") === 0) {
-  $reply->msgType = "TransferReply";
+  $sql = "select balance from card where PK_cardID = '$req->cardNo'";
+  $balance = mysqli_query($db,$sql);
+  if($balance > $req->amount){
+    $sql1 = "update card set balance = '$balance' - '$req->amount' where account = '$account'";
+    $sql2 = "update card set balance = '$balance' + '$req->amount' where account = '$req->toAcc'";
+    $reply->msgType = "TransferReply";
+    $reply->cardNo = $req->cardNo;
+    $reply->cred = "Withdraw Success";
+    $reply->fromAcc = $req->fromAcc;
+    $reply->toAcc = $req->toAcc;
+    $reply->amount = $req->amount;
+  }else(){
+    $reply->msgType = "TransferReply";
+    $reply->cred = "Not enough money";
+  }
 
 
 }
